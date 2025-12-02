7d95fd1f9bb91eb6b121f0483fed0449704b13c4
BradyFolster <folstb@rpi.edu>
2025-12-02 18:35:04 -0500

    Update projects.js

--- assets/js/projects.js
+++ assets/js/projects.js
@@ -4,7 +4,7 @@ $(document).ready(function()    {
         url: '/assets/json/projects.json',
         dataType: 'json',
         success: function(responseData, status){
-            const finalOutput = '';
+            var finalOutput = '';
             $.each(responseData, function(i, item) {
                 finalOutput += '<div class="projectbox"><h2>'+item.name+'</h2><img src="'+item.image+'" alt="'+item.name+'">'
                 finalOutput += '<a href="'+item.link+'" target="_blank">Link</a>'
