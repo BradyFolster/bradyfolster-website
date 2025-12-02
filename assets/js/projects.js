bbd90a52d3255c08d25235aeed8de9ab38f7138e
BradyFolster <folstb@rpi.edu>
2025-12-02 18:23:46 -0500

    Projects Page

---
+++ assets/js/projects.js
@@ -0,0 +1,16 @@
+$(document).ready(function()    {
+    $.ajax({
+        type: 'GET',
+        url: '/assets/json/projects.json',
+        dataType: 'json',
+        success: function(responseData, status){
+            const finalOutput = '';
+            $.each(responseData, function(i, item) {
+                finalOutput += '<div class="projectbox"><h2>'+item.name+'</h2><img src="'+item.image+'" alt="'+item.name+'">'
+                finalOutput += '<a href="'+item.link+'" target="_blank">Link</a>'
+                finalOutput += '<p>'+item.description+'</p>'
+            });
+            $('.pagecontent').append(finalOutput);
+        }
+    });
+});
