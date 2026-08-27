70eeec12636087a80eaed50eb986b8986184ad55
BradyFolster <folstb@rpi.edu>
2026-08-27 16:56:39 -0400

    css

--- assets/js/projects.js
+++ assets/js/projects.js
@@ -7,7 +7,7 @@ $(document).ready(function()    {
             var finalOutput = '';
             $.each(responseData, function(i, item) {
                 finalOutput += '<div class="projectbox"><a class="title-link" href="'+item.link+'"><h2>'+item.name+'</h2></a><a class="image-link" href="'+item.link+'"><img src="'+item.image+'" alt="'+item.name+'"></a>'
-                finalOutput += '<a href="'+item.github+'" target="_blank">GitHub</a>'
+                finalOutput += '<a class="github-link" href="'+item.github+'" target="_blank">GitHub</a>'
                 finalOutput += '<p>'+item.description+'</p></div>'
             });
             console.log(finalOutput);
